d768a5e12965466d60c376281a9138716a709bdf
KatKseniya <91549260+KatKseniya@users.noreply.github.com>
2021-10-25 01:09:56 +0300

    Update Journal.java

--- Journal.java
+++ Journal.java
@@ -1,28 +1,35 @@
 package by.epam.electronicVisitLog;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
+import java.util.GregorianCalendar;
 
 public class Journal
 {
-	public Date arrivalDate;
-	public Date departureDate;
-	
+
+
 	public static void main(String[] args)
 	{
-	ArrayList <Person> personList = new ArrayList<Person>();
-	personList.add(new Person("Men1", 1));
+		Calendar calendar = new GregorianCalendar(2021, 10 , 25);
+		ArrayList <Person> personList = new ArrayList<Person>();
+		Person men1 = new Person("Men1", 1, calendar.getTime(), null);
+		personList.add(men1);
 	}
+
 }
 
 class Person
 {
 	private String name;
 	private int id; // id of the pass
+	private Date arrivalDate;
+	private Date departureDate;
 
-	public Person(String name, int id)
+	public Person(String name, int id, Date arrivalDate, Date departureDate)
 	{
 		this.name = name;
 		this.id = id;
+		this.arrivalDate = arrivalDate;
+		this.departureDate = departureDate;
 	}
 }
