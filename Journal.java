a580161fd5a99dd81c9ba066a79df2f39f3dbfbd
KatKseniya <91549260+KatKseniya@users.noreply.github.com>
2021-10-24 23:18:37 +0300

    Add files via upload

---
+++ Journal.java
@@ -0,0 +1,24 @@
+package by.epam.electronicVisitLog;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Journal
+{
+	public Date arrivalDate;
+	public Date departureDate;
+	private List <Person> personLit = new ArrayList<Person>();
+
+}
+
+class Person
+{
+	private String name;
+	private int id; // id of the pass
+
+	public Person(String name, int id)
+	{
+		this.name = name;
+		this.id = id;
+	}
+}
