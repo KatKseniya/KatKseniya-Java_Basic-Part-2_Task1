c0e672004ecfee9a49a16dd7f4f037f6d663f779
KatKseniya <91549260+KatKseniya@users.noreply.github.com>
2021-10-25 18:51:49 +0300

    Add files via upload

---
+++ Note.java
@@ -0,0 +1,49 @@
+package by.epam.Note;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+class NoteBook
+{
+	private int id;
+	private String owner;
+
+	public NoteBook(int id, String owner)
+	{
+		this.id = id;
+		this.owner = owner;
+	}
+
+	public static ArrayList<Note> addNote (Note note)
+	{
+		ArrayList <Note> notes = new ArrayList<Note>();
+		notes.add(note);
+		return notes;
+	}
+
+
+	public static  ArrayList<Note> removeNote (Note note, int idOfNote)
+{
+	ArrayList <Note> notes = new ArrayList<Note>();
+	notes.remove(idOfNote);
+	return notes;
+}
+
+}
+
+public class Note
+{
+	private Date createDate;
+	private String text;
+	private boolean actual;
+
+	public Note(Date createDate, String text, boolean actual)
+	{
+		this.createDate = createDate;
+		this.text = text;
+		this.actual = actual;
+	}
+}
+
+
+
