cbc72c80c95aa6e3eada31979164bbe393da84b0
KatKseniya <91549260+KatKseniya@users.noreply.github.com>
2021-10-26 10:37:58 +0300

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -2,31 +2,8 @@ package by.epam.Hotel;
 import java.util.ArrayList;
 
 
-public class Hotel
-{
-	public int stars;
-	public int floors;
-	public int numberOfRooms;
-
-	public static ArrayList <Room> addRoom (Room room)
-	{
-		ArrayList <Room> rooms = new ArrayList<Room>();
-		rooms.add(room);
-		return rooms;
-}
-}
-
 class Room
 {
-	public int getId()
-	{
-		return id;
-	}
-
-	public void setId(int id)
-	{
-		this.id = id;
-	}
 
 	private int id;
 	private String level; // e.g. Economy, Luxury, Standart
@@ -45,30 +22,26 @@ class Room
 class Person
 {
 	private String name;
-	private int numberOfDays;
 	private int roomId;
 
-	public Person(String name, int numberOfDays, int roomId)
+	public Person(String name, int roomId)
 	{
 		this.name = name;
-		this.numberOfDays = numberOfDays;
 		this.roomId = roomId;
 	}
 }
 
-class CreateDataBaseOfRooms { // class for creating a database of hotel rooms
-	Hotel barselo = new Hotel();
-	ArrayList <Room> rooms = new ArrayList<Room>(new Hotel().addRoom(new Room(1, "Economy", 2, 50)));
-}
 
 class Order
 {
-	public static  ArrayList <Order> addRoom (Person men, Room room)
+	private Room room;
+	private Person person;
+	private int numberOfDays;
+
+	public Order(Room room, Person person, int numberOfDays)
 	{
-
-		// method for adding the appropriate room to the customer
-
-		return addRoom(men, room);
-
+		this.room = room;
+		this.person = person;
+		this.numberOfDays = numberOfDays;
 	}
 }
